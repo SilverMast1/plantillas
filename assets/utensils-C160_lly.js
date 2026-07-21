70aebc97c9f6582ccebf6a55a5fe55bb24f3283d
SilverMast1 <109107672+SilverMast1@users.noreply.github.com>
2026-07-21 18:28:07 +0000

    Deploying to gh-pages from @ SilverMast1/plantillas@f6c7fc6fb20a410dbd143b69514a4c8991ac1be8 🚀

---
+++ assets/utensils-C160_lly.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-7L-jYnes.js";/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("Copy",[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]]);/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Utensils",[["path",{d:"M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2",key:"cjf0a3"}],["path",{d:"M7 2v20",key:"1473qp"}],["path",{d:"M21 15V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7",key:"1ogz0v"}]]);export{c as C,a as U};
